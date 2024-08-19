1e9aab100aa87f76b4ad7195a628a7d989ae3245
Mydeen <Mydeen1193@gmail.com>
2024-08-19 10:17:58 +0530

    add anchor ang in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,10 +4,10 @@ import img from '../../public/Twitter Icon.png'
 export default function Footer(){
     return(
         <div className="footer">
-           <img src="/Twitter Icon.png" alt="twitter image" />
-           <img src="/Facebook Icon.png" alt="twitter image" />
-           <img src="/Instagram Icon.png" alt="twitter image" />
-           <img src="/GitHub Icon.png" alt="twitter image" />
+           <a href="##"><img src="/Twitter Icon.png" alt="twitter image" /></a>
+           <a href="##"><img src="/Facebook Icon.png" alt="twitter image" /></a>
+           <a href="##"><img src="/Instagram Icon.png" alt="twitter image" /></a>
+           <a href="##"><img src="/GitHub Icon.png" alt="twitter image" /></a>
         </div>
     )
 }
